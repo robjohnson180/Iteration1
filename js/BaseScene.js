6f3de96929d0448c865d5957232a9d27e179e8e0
robjohnson180 <43573071+robjohnson180@users.noreply.github.com>
2019-03-18 07:16:51 +0000

    Add files via upload

---
+++ js/BaseScene.js
@@ -0,0 +1,97 @@
+class BaseScene extends Phaser.Scene {
+    constructor(id) {
+        super(id);
+        this.id = id;
+        this.tileDataKey;
+        this.tileDataSource;
+        this.exit;
+    }
+    preload() {
+        this.load.tilemapTiledJSON(this.tileDataKey, this.tileDataSource);
+        this.load.image('flag', 'assets/flag.png');
+        this.load.image('landscape-tileset', 'assets/landscape-tileset.png');
+        this.load.image('spike', 'assets/spike.png');
+        this.load.image('skies', 'assets/skies.png');
+        this.load.image('player', 'assets/ralph.png');
+        /*this.load.spritesheet('player', 'assets/player.png', {
+            frameWidth: 32,
+            frameHeight: 32,
+            margin: 1,
+            spacing: 2
+        })*/
+        this.load.spritesheet('saves', 'assets/saveTilesheet.png', {
+            frameWidth: 16,
+            frameHeight: 16,
+        })
+    };
+    create() {
+        const map = this.make.tilemap({ key: this.tileDataKey });
+        const landscapeTileset = map.addTilesetImage('landscape-tileset');
+        const spikeTileset = map.addTilesetImage('spike');
+        const skiesTileset = map.addTilesetImage('skies');
+        this.background = map.createStaticLayer('backgroundLayer', [landscapeTileset, skiesTileset], 0, 0);
+        this.collideLayer = map.createStaticLayer('collideLayer', landscapeTileset, 0, 0);
+        this.foreground = map.createStaticLayer('foregroundLayer', landscapeTileset, 0, 0);
+        this.spikes = map.createStaticLayer('spikeLayer', spikeTileset, 0, 0);
+        //collision
+        this.collideLayer.setCollisionBetween(0, 1000);
+        this.collideLayer.setCollisionByProperty({collides:true});
+        this.spikes.setCollisionByProperty({collides:true});
+        //this.matter.world.convertTilemapLayer(this.collideLayer);
+        this.matter.world.convertTilemapLayer(this.spikes);
+        this.flag = this.matter.add.sprite(608, 96, 'flag');
+        this.flag.setStatic(true);
+        this.flag.label = 'flag';
+        this.player = new Player(this, 200, 0); //TODO Get from tiled
+        //this.player.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.matter.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.player.sprite,false,0.5,0.5);   
+        this.matter.world.on('collisionstart', this.handleCollision, this);
+        this.matter.world.on('collisionactive', this.handleCollision, this);
+
+    }
+    handleCollision(event) {
+        event.pairs.forEach(this.matchCollisionPair, this);
+    }
+
+    matchCollisionPair(pair) {
+        const bodyA = pair.bodyA;
+        const bodyB = pair.bodyB;
+        var playerObj = null;
+        let myPair = [null, null];
+        if (bodyA.gameObject && bodyA.gameObject.label) {
+            this.sortCollisionObjects(bodyA.gameObject.label, myPair);
+        }
+        if (bodyB.gameObject && bodyB.gameObject.label) {
+            this.sortCollisionObjects(bodyB.gameObject.label, myPair);
+        }
+        if (myPair[0] == 'player' && myPair[1] == 'flag') {
+            this.changeScene();
+        }
+    }
+    sortCollisionObjects(label, arr) {
+        switch (label) {
+            case 'player':
+                arr[0] = 'player';
+                break
+            case 'flag':
+                arr[1] = 'flag';
+                break
+        }
+    }
+
+
+    update(time, delta) {
+        this.player.update();
+
+    }
+    changeScene() {
+        switch (this.id) {
+            case 'TutScene':
+                this.scene.start('sceneA');
+                break
+        }
+    }
+
+}
---
+++ js/main.js
@@ -0,0 +1,52 @@
+console.log("loaded");
+
+var config = {
+    type: Phaser.AUTO,
+    width: 512,
+    height: 512,
+    scene: {
+
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            debug: false,
+            gravity: { y: 0.5 }
+        }
+    },
+
+    plugins: {
+        scene: [{
+            plugin: PhaserMatterCollisionPlugin,
+            key: 'matterCollision',
+            mapping: 'matterCollision'
+        }]
+    },
+
+    scene: [TutScene],
+    callbacks: {
+        postBoot: function () {
+            resize();
+        }
+    },
+    pixelArt: false
+}
+
+var game = new Phaser.Game(config);
+
+window.addEventListener('resize', resize, false);
+function resize() {
+    var canvas = document.querySelector('canvas');
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+
+    if (windowRatio < gameRatio) {
+        canvas.style.width = windowWidth + 'px';
+        canvas.style.height = (windowWidth / gameRatio) + 'px';
+    } else {
+        canvas.style.width = (windowHeight * gameRatio) + 'px';
+        canvas.style.height = windowHeight + 'px';
+    }
+}
